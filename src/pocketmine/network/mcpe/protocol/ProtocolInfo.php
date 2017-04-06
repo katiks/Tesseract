db3e23dd3dfd700ddecb478b616a017f0f2c208e
Nathaniel <nfgamermc@outlook.com>
2017-04-06 12:57:55 -0300

    Make version look more accurate

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -32,7 +32,7 @@ interface ProtocolInfo{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 110;
-	const MINECRAFT_VERSION = "v1.1.0.0 beta";
+	const MINECRAFT_VERSION = "v1.1.0 beta";
 	const MINECRAFT_VERSION_NETWORK = "1.1.0.0";
 
 	const LOGIN_PACKET = 0x01;
