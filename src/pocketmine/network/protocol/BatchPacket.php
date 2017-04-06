5d70424666dacf05855ed6d42a70f49292957571
caspervanneck <wesleyvanneck1012@gmail.com>
2017-04-06 15:21:39 +0200

    Update BatchPacket.php

--- src/pocketmine/network/protocol/BatchPacket.php
+++ src/pocketmine/network/protocol/BatchPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,7 +15,7 @@
  *
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
- * 
+ *
  *
 */
 
@@ -24,21 +24,57 @@ namespace pocketmine\network\protocol;
 #include <rules/DataPacket.h>
 
 
+use pocketmine\network\NetworkSession;
+
 class BatchPacket extends DataPacket{
-	const NETWORK_ID = Info::BATCH_PACKET;
+	const NETWORK_ID = 0xfe;
 
 	public $payload;
 
+	public function canBeBatched() : bool{
+		return false;
+	}
+
+	public function canBeSentBeforeLogin() : bool{
+		return true;
+	}
+
 	public function decode(){
-		$this->payload = $this->getString();
+		$this->payload = $this->get(true);
 	}
 
 	public function encode(){
-//		$this->reset();
-        $this->offset = 0;
-//		$this->putString($this->payload);
-        $this->buffer = $this->payload;
-        $this->isZiped = true;
+		$this->reset();
+		$this->put($this->payload);
+	}
+
+	public function handle(NetworkSession $session) : bool{
+		if(strlen($this->payload) < 2){
+			throw new \InvalidStateException("Not enough bytes in payload, expected zlib header");
+		}
+
+		$str = zlib_decode($this->payload, 1024 * 1024 * 64); //Max 64MB
+		$len = strlen($str);
+
+		if($len === 0){
+			throw new \InvalidStateException("Decoded BatchPacket payload is empty");
+		}
+
+		$this->setBuffer($str, 0);
+
+		$network = $session->getServer()->getNetwork();
+		while(!$this->feof()){
+			$buf = $this->getString();
+			$pk = $network->getPacket(ord($buf{0}));
+			/*if(!$pk->canBeBatched()){
+				throw new \InvalidArgumentException("Received invalid " . get_class($pk) . " inside BatchPacket");
+			}*/
+
+			$pk->setBuffer($buf, 1);
+			$session->handleDataPacket($pk);
+		}
+
+		return true;
 	}
 
 }
